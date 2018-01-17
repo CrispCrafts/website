03a17d53339c25ec2e98ca47da79bd2963e97acb
Cristian Ponce <cristianrponce06@gmail.com>
2018-01-17 01:11:22 -0800

    Updated Projects

--- src/App.js
+++ src/App.js
@@ -13,9 +13,14 @@ class App extends Component {
         <p className="App-intro">
           Site Coming Soon...
           <br/>
-          <a href="http://colors.crispcrafts.com">color-picker</a>
-          <br/>
-          <a href="http://reactive-mirror.crispcrafts.com">reactive-mirror</a>
+          <div style={{textAlign: 'left', marginLeft: '30px'}}>
+            <h1><a href="https://colors.crispcrafts.com">colors</a></h1>
+            <p>A set of color tools</p>
+            <h1><a href="https://characters.crispcrafts.com">characters</a></h1>
+            <p>A set of character tools</p>
+            <h1><a href="https://github.com/cristian006/ReactiveMirror">reactive-mirror</a></h1>
+            <p>A modular react + electron app meant to run on Raspberry Pi 3 for a DIY Smart Mirror</p>
+          </div>
         </p>
       </div>
     );
